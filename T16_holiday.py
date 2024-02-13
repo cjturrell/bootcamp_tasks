061bda0f133d26ddf1cec3c9d65365574e8d9d9b
cjturrell <caitlin.turrell@hotmail.com>
2024-02-13 13:48:07 +0000

    Updated task comments and functions

--- T16_holiday.py
+++ T16_holiday.py
@@ -2,8 +2,14 @@
 A program to determine a users total holiday cost.
 Includes plane cost, hotel cost and car rental cost.
 """
-
-def plane_cost():
+# Functions ==================================================================
+# add docstring for functions!!! 
+def plane_cost(city_flight):
+    """
+    The function calculates the cost of a plane ticket based on the 
+    destination city.
+    :return: the value of the variable "flight_total".
+    """
     if city_flight == "Ireland":
         flight_total = 50
     elif city_flight == "Paris":
@@ -16,31 +22,54 @@ def plane_cost():
         print("Destination not recognized")
     return flight_total
 
-def hotel_cost():
-    hotel_total = num_nights * 50
+def hotel_cost(num_nights, hotel_cost=50):
+    """
+    The function calculates the total cost of a hotel stay based on the number 
+    of nights.
+    :return: the total cost of the hotel stay.
+    """
+    hotel_total = num_nights * hotel_cost
     return hotel_total
 
-def car_rental():
-    car_total = rental_days * 25
+def car_rental(rental_days, car_rental_cost=25):
+    """
+    The function calculates the total cost of renting a car based on the 
+    number of rental days.
+    :return: The variable `car_total` is being returned.
+    """
+    car_total = rental_days * car_rental_cost
     return car_total
 
-def holiday_cost():
-    holiday_total = plane_cost() + hotel_cost() + car_rental()
+def holiday_cost(plane_cost, hotel_cost, car_rental):
+    """
+    The function calculates the total cost of a holiday by adding the costs of
+    a plane ticket, hotel stay, and car rental.
+    :return: the total cost of the holiday, which is calculated by adding the 
+    costs of the plane, hotel, and car rental.
+    """
+    holiday_total = (plane_cost(city_flight) + 
+                    hotel_cost(num_nights, hotel_cost=50) + 
+                    car_rental(rental_days, car_rental_cost=25))
     return holiday_total
 
+# Welcome message ============================================================
 print("-"*50)
 print("Welcome to the holiday cost calculator!")
 print("-"*50)
 
+
 print("City destinations are: \nIreland \nParis \nAmsterdam \nBerlin")
+# Ensure user input is valid for flight destination
 while True:
-    city_flight = (input("Which destination are you flying to:\t")).capitalize()
+    city_flight = (input("Which destination are you flying to"
+                         ":\t")).capitalize()
     if city_flight == "Ireland" or city_flight == "Paris" \
         or city_flight == "Amsterdam" or city_flight == "Berlin":
         break
     else:
         print("Destination not recognized")
 
+# Ensure user input is numerical for number of hotel nights
 while True:
     num_nights = input("Number of nights at the hotel:\t")
     if num_nights.isnumeric():
@@ -49,6 +78,7 @@ while True:
     else:
         print("Please type a number")
 
+# Ensure user input is numerical for number or car rental days
 while True:
     rental_days = input("Number of days to rent a car:\t")
     if rental_days.isnumeric():
@@ -57,9 +87,12 @@ while True:
     else:
         print("Please type a number")
 
+# Display all information in a readable format
 print("-"*50)
-print(f"Plane to {city_flight}: \t£{"%.2f"%plane_cost()}\
-      \nHotel for {num_nights} nights: \t£{"%.2f"%hotel_cost()}\
-      \nCar rental for {rental_days} days: \t£{"%.2f"%car_rental()}\
-      \nHoliday Total: £{"%.2f"%holiday_cost()}")
+print(f"Plane to {city_flight}: \t£{"%.2f"%plane_cost(city_flight)}"
+      f"\nHotel for {num_nights} nights: \t£{"%.2f"%hotel_cost(num_nights)}"
+      f"\nCar rental for {rental_days} days: "
+        f"\t£{"%.2f"%car_rental(rental_days)}"
+      f"\n\nHoliday Total: "
+        f"£{"%.2f"%holiday_cost(plane_cost, hotel_cost, car_rental)}")
 print("-"*50)
